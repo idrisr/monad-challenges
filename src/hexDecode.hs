72bd75c3d7946554760317c0941f9c55e3e88d61
Idris Raja <idris.raja@gmail.com>
2023-06-14 18:50:30 -0500

    delete stuff i dont need

---
+++ src/hexDecode.hs
@@ -0,0 +1,5 @@
+import Numeric (readHex)
+import Data.List.Split (chunksOf)
+
+hexDecode :: String -> String
+hexDecode = map (toEnum . fst . head . readHex) . chunksOf 2
---
+++ src/template.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE MonadComprehensions #-}
+{-# LANGUAGE RebindableSyntax  #-}
+
+module Set1 where
+
+import MCPrelude
