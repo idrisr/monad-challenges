df1014f95a1ad7a952e75df72e6afc836fd5731a
Idris Raja <idris.raja@gmail.com>
2023-06-15 13:48:45 -0500

    2.2 Build a library of things that can fail

--- challenges/Maybe.hs
+++ challenges/Maybe.hs
@@ -13,3 +13,38 @@ instance Eq a => Eq (Maybe a) where
     Just a == Just b = a == b
     Nothing == Nothing = True
     _ == _ = False
+
+headMay :: [a] -> Maybe a
+headMay [] = Nothing
+headMay (x : _) = Just x
+
+tailMay :: [a] -> Maybe [a]
+tailMay [] = Nothing
+tailMay (_ : xs) = Just xs
+
+lookupMay :: Eq a => a -> [(a, b)] -> Maybe b
+lookupMay _ [] = Nothing
+lookupMay a ((b, c) : xs) =
+    if a == b
+        then Just c
+        else lookupMay a xs
+
+divMay :: (Eq a, Fractional a) => a -> a -> Maybe a
+divMay x y =
+    if y == 0
+        then Nothing
+        else Just $ x / y
+
+maximumMay :: Ord a => [a] -> Maybe a
+maximumMay [] = Nothing
+maximumMay [x] = Just x
+maximumMay (x : xs) = case maximumMay xs of
+    Just a -> Just $ max a x
+    Nothing -> Nothing
+
+minimumMay :: Ord a => [a] -> Maybe a
+minimumMay [] = Nothing
+minimumMay [x] = Just x
+minimumMay (x : xs) = case minimumMay xs of
+    Just a -> Just $ min a x
+    Nothing -> Nothing
